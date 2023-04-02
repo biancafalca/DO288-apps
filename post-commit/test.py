4aa9152720b7b70294eda1aa802f289e01eb130a
Student User <student@workstation.lab.example.com>
2023-04-02 06:18:18 -0400

    Updated python to curl

--- post-commit/test.py
+++ post-commit/test.py
@@ -1,6 +1,5 @@
-import commands
+import os
 
-status, output = commands.getstatusoutput("curl -s -S -i -X POST http://builds-for-managers-ykezyp-post-commit.apps.eu46a.prod.ole.redhat.com/api/build -f -d 'developer=Dev&git=Git&project=proj'")
+os.system("sh script.sh")
 
-print(output)
 print("This line will be printed.")
