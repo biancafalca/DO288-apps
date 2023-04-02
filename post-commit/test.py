544d11e24a34fc334b2e93ae15a07f6d1c0eb1c6
Student User <student@workstation.lab.example.com>
2023-04-02 06:06:31 -0400

    Updated python

--- post-commit/test.py
+++ post-commit/test.py
@@ -1,1 +1,6 @@
+import commands
+
+status, output = commands.getstatusoutput("curl -s -S -i -X POST http://builds-for-managers-ykezyp-post-commit.apps.eu46a.prod.ole.redhat.com/api/build -f -d 'developer=Dev&git=Git&project=proj'")
+
+print(output)
 print("This line will be printed.")
